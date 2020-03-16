91660e9fd943a9604edd7727af1bd7a5c8e1ed05
anz2 <anzor.gozalishvili@maxinai.com>
2020-03-16 17:37:53 +0400

    Set workers to 1.

--- run_bert_service.sh
+++ run_bert_service.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bert-serving-start -num_worker=2 -max_seq_len=None -model_dir ./cased_L-12_H-768_A-12
+bert-serving-start -num_worker=1 -max_seq_len=None -model_dir ./cased_L-12_H-768_A-12
