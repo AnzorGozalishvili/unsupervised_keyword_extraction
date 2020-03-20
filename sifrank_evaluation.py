44906d2f64bc5a467373b1693a91934e35328933
anz2 <anzor.gozalishvili@maxinai.com>
2020-03-20 21:00:05 +0400

    Update sifrank evaluation script.

--- sifrank_evaluation.py
+++ sifrank_evaluation.py
@@ -94,7 +94,7 @@ def get_model(model_name, **kwargs):
         return EmbedRankTransformersWrapper(**kwargs)
     elif model_name == 'EmbedRank':
         return EmbedRankWrapper(**kwargs)
-    elif model_name == 'SIFRank':
+    elif model_name == 'SIFRank' or 'SIFRankPlus':
         return SIFRankWrapper(**kwargs)
 
 
