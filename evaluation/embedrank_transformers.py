d4ee43e43d99318e679b9218bd36fac7054d388b
anz2 <anzor.gozalishvili@maxinai.com>
2020-03-28 19:37:04 +0400

    Remove parent class of embedrank transformers from trec evaluation.

--- evaluation/embedrank_transformers.py
+++ evaluation/embedrank_transformers.py
@@ -6,9 +6,21 @@ from helpers import read_json
 from keyword_extraction.helpers import init_keyword_extractor
 
 
-class EmbedRankTransformers(object):
-    def __init__(self):
-        self.model = None
+class EmbedRankBERT(object):
+    def __init__(self, numOfKeywords, pathData, dataset_name, normalization):
+        super().__init__()
+        self.__lan = getlanguage(pathData + "/Datasets/" + dataset_name)
+        self.__numOfKeywords = numOfKeywords
+        self.__dataset_name = dataset_name
+        self.__normalization = normalization
+        self.__pathData = pathData
+        self.__pathToDFFile = self.__pathData + "/Models/Unsupervised/dfs/" + self.__dataset_name + '_dfs.gz'
+        self.__pathToDatasetName = self.__pathData + "/Datasets/" + self.__dataset_name
+        self.__keywordsPath = f"{self.__pathData}/Keywords/{self.__class__.__name__}/{self.__dataset_name}"
+        self.__outputPath = self.__pathData + "/conversor/output/"
+        self.__algorithmName = f"{self.__class__.__name__}"
+
+        self.model = init_keyword_extractor(read_json('evaluation/config/embedrank_bert_as_a_service.json'))
 
     def LoadDatasetFiles(self):
         # Gets all files within the dataset fold
@@ -67,24 +79,7 @@ class EmbedRankTransformers(object):
                          self.__dataset_name, self.__algorithmName)
 
 
-class EmbedRankBERT(EmbedRankTransformers):
-    def __init__(self, numOfKeywords, pathData, dataset_name, normalization):
-        super().__init__()
-        self.__lan = getlanguage(pathData + "/Datasets/" + dataset_name)
-        self.__numOfKeywords = numOfKeywords
-        self.__dataset_name = dataset_name
-        self.__normalization = normalization
-        self.__pathData = pathData
-        self.__pathToDFFile = self.__pathData + "/Models/Unsupervised/dfs/" + self.__dataset_name + '_dfs.gz'
-        self.__pathToDatasetName = self.__pathData + "/Datasets/" + self.__dataset_name
-        self.__keywordsPath = f"{self.__pathData}/Keywords/{self.__class__.__name__}/{self.__dataset_name}"
-        self.__outputPath = self.__pathData + "/conversor/output/"
-        self.__algorithmName = f"{self.__class__.__name__}"
-
-        self.model = init_keyword_extractor(read_json('evaluation/config/embedrank_bert_as_a_service.json'))
-
-
-class EmbedRankSentenceBERT(EmbedRankTransformers):
+class EmbedRankSentenceBERT(object):
     def __init__(self, numOfKeywords, pathData, dataset_name, normalization):
         super().__init__()
         self.__lan = getlanguage(pathData + "/Datasets/" + dataset_name)
@@ -99,3 +94,59 @@ class EmbedRankSentenceBERT(EmbedRankTransformers):
         self.__algorithmName = f"{self.__class__.__name__}"
 
         self.model = init_keyword_extractor(read_json('evaluation/config/embedrank_sentence_bert.json'))
+
+    def LoadDatasetFiles(self):
+        # Gets all files within the dataset fold
+        listFile = LoadFiles(self.__pathToDatasetName + '/docsutf8/*')
+        print(f"\ndatasetID = {self.__dataset_name}; Number of Files = "
+              f"{len(listFile)}; Language of the Dataset = {self.__lan}")
+        return listFile
+
+    def CreateKeywordsOutputFolder(self):
+        # Set the folder where keywords are going to be saved
+        CreateKeywordsFolder(self.__keywordsPath)
+
+    def runSingleDoc(self, doc):
+        try:
+            # read raw document
+            with open(doc, 'r') as doc_reader:
+                doc_text = doc_reader.read()
+
+            # extract keywords
+            keywords, relevance = self.model.run(doc_text)
+            keywords = [(keyword, score) for (keyword, _, _), score in zip(keywords, relevance)]
+        except:
+            keywords = []
+
+        return keywords
+
+    def runMultipleDocs(self, listOfDocs):
+        self.CreateKeywordsOutputFolder()
+
+        for j, doc in enumerate(listOfDocs):
+            # docID keeps the name of the file (without the extension)
+            docID = '.'.join(os.path.basename(doc).split('.')[0:-1])
+
+            keywords = self.runSingleDoc(doc)
+
+            # Save the keywords; score (on Algorithms/NameOfAlg/Keywords/NameOfDataset
+            with open(os.path.join(self.__keywordsPath, docID), 'w', encoding="utf-8") as out:
+                for (key, score) in keywords:
+                    out.write(f'{key} {score}\n')
+
+            # Track the status of the task
+            print(f"\rFile: {j + 1}/{len(listOfDocs)}", end='')
+
+        print(f"\n100% of the Extraction Concluded")
+
+    def ExtractKeyphrases(self):
+        # print(f"\n------------------------------Compute DF--------------------------")
+        # self.ComputeDocumentFrequency()
+
+        print(f"\n\n-----------------Extract Keyphrases--------------------------")
+        listOfDocs = self.LoadDatasetFiles()
+        self.runMultipleDocs(listOfDocs)
+
+    def Convert2Trec_Eval(self, EvaluationStemming=False):
+        Convert2TrecEval(self.__pathToDatasetName, EvaluationStemming, self.__outputPath, self.__keywordsPath,
+                         self.__dataset_name, self.__algorithmName)
